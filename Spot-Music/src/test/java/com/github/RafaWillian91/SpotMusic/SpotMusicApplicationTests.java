d2fdd72770240d96630a1eb6f0886bc24f37b303
rafaw <rafa.willian91@gmail.com>
2023-09-22 21:01:32 -0300

    First Commit

---
+++ Spot-Music/src/test/java/com/github/RafaWillian91/SpotMusic/SpotMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.RafaWillian91.SpotMusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
