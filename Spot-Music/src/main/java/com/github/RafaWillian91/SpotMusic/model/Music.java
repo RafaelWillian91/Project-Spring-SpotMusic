973030412239e2e4f9922a27e9aa8888393eb58d
rafaw <rafa.willian91@gmail.com>
2023-09-23 02:34:59 -0300

    domain criado

---
+++ Spot-Music/src/main/java/com/github/RafaWillian91/SpotMusic/model/Music.java
@@ -0,0 +1,62 @@
+package com.github.RafaWillian91.SpotMusic.model;
+
+import org.hibernate.validator.constraints.Range;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "musics")
+public class Music {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    @Size(min = 2, max = 60)
+    @Column(nullable = false, length = 50)
+    private String title;
+    @NotBlank
+    @Size(min = 2, max = 60)
+    @Column(nullable = false, length = 50)
+    private String band;
+    @Range(min = 0, max = 10)
+    @Column(nullable = false)
+    private int score;
+    @ManyToOne
+    @JoinColumn(name = "id_playlists_fk")
+    private Playlist playlists;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBand() {
+        return band;
+    }
+
+    public void setBand(String band) {
+        this.band = band;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
---
+++ Spot-Music/src/main/java/com/github/RafaWillian91/SpotMusic/model/Playlist.java
@@ -0,0 +1,58 @@
+package com.github.RafaWillian91.SpotMusic.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Entity
+@Table(name = "playlists")
+public class Playlist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(min = 2, max = 60)
+    @Column(nullable = false, length = 60)
+    private String name;
+    @NotBlank
+    @Column(nullable = false)
+    private String description;
+    @OneToMany(mappedBy = "playlists", cascade = CascadeType.ALL)
+    private List<Music> listMusic;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Music> getListMusic() {
+        return listMusic;
+    }
+
+    public void setListMusic(List<Music> listMusic) {
+        this.listMusic = listMusic;
+    }
+}
